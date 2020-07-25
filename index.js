030f1b226177b1ce03b289a97ba02d12ab26a5f1
Jonathan <Jonnyghan@gmail.com>
2020-07-25 13:29:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let element = document.querySelector("main#main");
+element.remove();
+let newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+let element2 = document.querySelector("h1");
+element2.id = "victory";
+element2.nodeName = "H1";
+element2.innerHTML = `Jonathan is the champion`;
